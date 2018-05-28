5ff62e4c56791d9cfc1797193ac011f5f250466a
thluiz <contato@thluiz.com>
2018-05-28 00:05:45 -0300

    fixing firebase key parser

--- src/services/firebase-service.js
+++ src/services/firebase-service.js
@@ -8,7 +8,7 @@ admin.initializeApp({
         "type": "service_account",
         "project_id": process.env.FIREBASE_PROJECT_ID,
         "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace("\\n", "\n"),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
         "client_id": process.env.FIREBASE_CLIENT_ID,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
