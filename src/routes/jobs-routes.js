31234e3130ba741516d2967e0b06afde8911d4c9
thluiz <contato@thluiz.com>
2018-07-04 05:50:25 -0300

    email tests

--- src/routes/jobs-routes.js
+++ src/routes/jobs-routes.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const jobs_service_1 = require("../services/jobs-service");
+const sgMail = require('@sendgrid/mail');
 function routes(app) {
     app.get("/api/hourly-jobs", (req, res, next) => __awaiter(this, void 0, void 0, function* () {
         let result = yield jobs_service_1.JobsService.execute_hourly_jobs();
@@ -17,6 +18,33 @@ function routes(app) {
     app.get("/api/cleanup-sessions", (req, res, next) => __awaiter(this, void 0, void 0, function* () {
         let result = yield jobs_service_1.JobsService.cleanup_sessions();
         res.send(result);
+    }));
+    app.get("/api/test-email", (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: 'moychiyausi.myvt@gmail.com',
+            from: 'contato@myvtmi.im',
+            subject: 'Sending with SendGrid!',
+            text: 'Siu Nim Tau, Cham Kiu, Biu Ji...',
+            html: '<strong>Tan Sau</strong>, Bong Sau...',
+        };
+        sgMail.send(msg)
+            .then(r2 => {
+            console.log(r2);
+            res.send("ok v3!");
+        })
+            .catch(error => {
+            //Log friendly error
+            console.error(error.toString());
+            //Extract error msg
+            const { message, code, response } = error;
+            console.log(code);
+            //Extract response msg
+            const { headers, body } = response;
+            console.log(headers);
+            console.log(body);
+            res.send("arg!");
+        });
     }));
 }
 exports.routes = routes;
--- src/routes/jobs-routes.ts
+++ src/routes/jobs-routes.ts
@@ -1,7 +1,10 @@
-import { AzureTableService } from './../services/azure-tables-service';
 import { JobsService } from "../services/jobs-service";
 
+const sgMail = require('@sendgrid/mail');
+
 export function routes(app) {
+    
+
     app.get("/api/hourly-jobs", async (req, res, next) => {                
         let result = await JobsService.execute_hourly_jobs();
         
@@ -12,5 +15,40 @@ export function routes(app) {
         let result = await JobsService.cleanup_sessions();
         
         res.send(result);
+    }); 
+
+    app.get("/api/test-email", async (req, res, next) => {                
+        
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+        to: 'moychiyausi.myvt@gmail.com',
+        from: 'contato@myvtmi.im',
+        subject: 'Sending with SendGrid!',
+        text: 'Siu Nim Tau, Cham Kiu, Biu Ji...',
+        html: '<strong>Tan Sau</strong>, Bong Sau...',
+        };
+
+        sgMail.send(msg)
+                .then(r2 =>  { 
+                    console.log(r2);
+                    res.send("ok v3!");
+                })
+                .catch(error => {
+
+                    //Log friendly error
+                    console.error(error.toString());
+                
+                    //Extract error msg
+                    const {message, code, response} = error;
+
+                    console.log(code);
+
+                    //Extract response msg
+                    const {headers, body} = response;
+
+                    console.log(headers);
+                    console.log(body);
+                    res.send("arg!");
+                });                
     }); 
 }
